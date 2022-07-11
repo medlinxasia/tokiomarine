2057819073b9b4521a4abcdb13aad0676ce14978
Steven <steven.medlinx@gmail.com>
2022-07-11 17:06:40 +0700

    update

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,4 +1,4 @@
-if('serviceWorker' in navigator){    navigator.serviceWorker.register("./sw.js").then(reg => {
+if('serviceWorker' in navigator){    navigator.serviceWorker.register("./assets/js/sw.js").then(reg => {
         console.log("Successfully Registered");
     }).catch(err => {
         console.log("Error while registering SW");
